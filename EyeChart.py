fc16325de8830343150b0b462d70b7df3b8aa028
aurel84 <johnhawkins3d@gmail.com>
2023-10-02 21:44:22 -0700

    Update EyeChart.py
    add self-destruct / reload, size changes, font types

--- EyeChart.py
+++ EyeChart.py
@@ -1,36 +1,39 @@
-from tkinter import *
+#!/usr/bin/env python3
+import tkinter as tk
+from tkinter import font
 import random
 import string
 
 
-root = Tk()
-root.geometry('660x1100')
+root = tk.Tk()
+root.geometry('840x1300')
 root.title('Eye Exam')
-menubar = Menu(root)
-choices = Menubutton(menubar)
-root.configure(background='White', menu=menubar)
+root.resizable(False, False)
+root.configure(background='White')
+root.grid_propagate(0)
 
-root.columnconfigure(index=0)
-root.columnconfigure(index=1)
-root.columnconfigure(index=2)
-root.columnconfigure(index=3)
-root.columnconfigure(index=4)
-root.rowconfigure(index=1)
 
-letter_size = 192
-char_row_num = 1
-sub_row = 1
+for i in range(2):
+    root.columnconfigure(i, weight=1)
+for i in range(11):
+    root.rowconfigure(i, weight=1)
 
+font_type_list = ['Courier', 'FreeSerif', 'Gothic', 'Purisa', 'Waree']
+font_size_list = []
+grid_list = [] 
+
+font_type = tk.StringVar()
+font_type.set('FreeSerif')
+font_size = 160
 
 class char:
 
     def __init__(self, char, case, font):
-        self.letter = char
+        self.char = char
         self.case = case
         self.font = font
     
     def do_rand_letter(char):
-        global create_letter
 
         if char == 'Upper':
             create_letter = random.choice(string.ascii_uppercase)
@@ -42,37 +45,21 @@ class char:
         return create_letter
 
     def do_rand_number():
-        global create_number
 
         create_number = random.randint(0, 9)
 
         return create_number
     
     def do_char_list():
-        global char_list
 
         char_list = []
 
         for i in range(45):
             char_list.append(char.do_rand_letter('Upper'))
 
-        def add_spaces(arg, x):
-            return int(x)*" "+arg+int(x)*" "
-
-        char_list[0:1] = [str(add_spaces(x, .1)) for x in char_list[0:1]]
-        char_list[1:3] = [str(add_spaces(x, .3)) for x in char_list[1:3]]
-        char_list[3:6] = [str(add_spaces(x, .6)) for x in char_list[3:6]]
-        char_list[6:10] = [str(add_spaces(x, 1)) for x in char_list[6:10]] 
-        char_list[10:15] = [str(add_spaces(x, 1.5)) for x in char_list[10:15]] 
-        char_list[15:21] = [str(add_spaces(x, 2.1)) for x in char_list[15:21]] 
-        char_list[21:28] = [str(add_spaces(x, 2.8)) for x in char_list[21:28]] 
-        char_list[28:36] = [str(add_spaces(x, 3.6)) for x in char_list[28:36]]
-        char_list[36:45] = [str(add_spaces(x, 4.5)) for x in char_list[36:45]]
-
         return char_list
 
     def do_grid_index():
-        global grid_index
 
         grid_index = [1, 3, 6, 10, 15, 21, 28, 36, 45]
         grid_output = []
@@ -97,112 +84,129 @@ class interface:
 
     def do_inputs(button_text, column_input, row_input, sticky_input, padx_input, command_input):
 
-        button = Button(root, text=button_text, foreground='Black', background='White', command=command_input, border=None)
+        button = tk.Button(root, text=button_text, foreground='Black', background='White', command=command_input, border=None)
         button.grid(column=column_input, row=row_input, sticky=sticky_input, padx=padx_input)
 
-        return
-
     def do_dropdown(dropdown_text, column_input, row_input, sticky_input, padx_input):
+        global font_size, font_type, font_type_list
 
-        dropdown = Menubutton(root, text=dropdown_text, background='White', activebackground='Blue', relief='raised')
-        dropdown.menu = Menu(dropdown)  
-        dropdown["menu"]= dropdown.menu  
-
-        dropdown.menu.add_checkbutton(label='Caligri', background='White')
-        dropdown.menu.add_checkbutton(label='Times New Roman', background='White')
-
+        dropdown = tk.OptionMenu(root, font_type, *(font_type_list))
         dropdown.grid(column=column_input, row=row_input, sticky=sticky_input, padx=padx_input)
 
-        return
+        return font_type.get()
 
-    def do_labels(font_text, column_input, row_input, sticky_input):
+    def do_labels(font_text, foreground_input, background_input, column_input, row_input, sticky_input):
 
-        pick_font = Label(root, text=font_text, foreground='Black', background='white')
+        pick_font = tk.Label(root, text=font_text, foreground=foreground_input, background=background_input)
         pick_font.grid(column=column_input, row=row_input, sticky=sticky_input)
-
-        return
 
     def do_graphics():
 
-        green_line = Canvas(root, background='Green', height=5)
-        green_line.grid(column=2, row=5)
+        green_line = tk.Canvas(root, background='Green', height=5)
+        green_line.grid(column=1, row=6)
 
-        red_line = Canvas(root, background='Red', height=5)
-        red_line.grid(column=2, row=9)
+        red_line = tk.Canvas(root, background='Red', height=5)
+        red_line.grid(column=1, row=10)
 
-    def do_chart_grid():
-        global chart_grid, letter_size, char_row_num, sub_row
+    def do_frame(width_input, height_input, columnspan_input, column_input, row_input):
+
+        eye_frame = tk.Frame(root, background="light blue", width=width_input, height=height_input)
+        eye_frame.grid(columnspan=columnspan_input, column=column_input, row=row_input, sticky='N')
+
+    def do_chart_row_grid(font_size):
+        global grid_list, font_size_list
+
         
-        gen_row = char.do_char_list()
+        sub_row = 2
+        char_row_num = 1
 
         for indice in char.do_grid_index():
 
             indice = str(indice).strip('{').strip('}').strip('[').strip(']').replace(',', '').replace("'",'')
 
-            if char_row_num < 10:
-
-                chart_number = Label(root, text=char_row_num, font='Calibri 20', foreground='Black', background='White')
-                chart_number.grid(column=0, row=sub_row)
-
-            chart_grid = Label(root, text=indice, font='Calibri '+str(letter_size), foreground='Black', background='White')
-            chart_grid.grid(column=2, row=sub_row, pady=5)
+            chart_grid = tk.Label(root, text=indice, font=str(f'{font_type.get()} {font_size}'), foreground='Black', background='White')
+            chart_grid.grid(column=1, row=sub_row, sticky='S', padx=None)
 
             char_row_num += 1
             sub_row += 1
 
-            if sub_row == 5:
+            if sub_row == 6 or sub_row == 10:
                 sub_row += 1
 
-            elif sub_row == 9:
+            font_size_list.append(font_size)
+            grid_list.append(chart_grid)
+
+            font_size = int(font_size)//1.3
+            font_size = int(font_size)
+
+
+        print(font_size_list)
+
+        return font_size, grid_list, font_size_list, font_type
+
+    def do_row_numbers():
+
+        sub_row = 2
+        chart_row_num = 1
+
+        for x in range(9):
+
+            chart_number = tk.Label(root, text=chart_row_num, font='Calibri 20', foreground='Black', background='White')
+            chart_number.grid(column=0, row=sub_row, sticky='W')
+
+            chart_row_num += 1
+            sub_row += 1
+
+            if sub_row == 6 or sub_row == 10:
                 sub_row += 1
 
-            letter_size = int(letter_size)//1.5
-            letter_size = int(letter_size)
+    def do_font_size(outcome):
+        global grid_list, font_size_list, font_size, font_type
 
-        return chart_grid, sub_row, letter_size, gen_row, char_row_num
+        if outcome == 0:
+
+            if max(font_size_list) < 170:
+                for i in range(len(font_size_list)):
+                    font_size_list[i] +=2
+
+        elif outcome == 1:
+
+            if min(font_size_list) > 1:
+                for i in range(len(font_size_list)):
+                    font_size_list[i] -=2
+
+        font_size_list = list(map(int, font_size_list))
+        print(font_type.get())
+
+        for i, x in (zip(grid_list, font_size_list)):
+            i.configure(font=(font_type.get(), x))
+
+        print(font_size_list)
+
+        return
+
+    def do_reload_chart():
+
+        for i in root.winfo_children():
+            if isinstance(i, tk.Label):
+                i.destroy()
+
+        do_eye_exam()
 
 
-def refresh():
-    global root, letter_size, char_row_num, sub_row, choices
-
-    root.destroy()
-    root = Tk()
-    root.geometry('660x1100')
-    root.title('Eye Exam')
-    menubar = Menu(root)
-    choices = Menubutton(menubar)
-    root.configure(background='White', menu=menubar)
-
-    root.columnconfigure(index=0)
-    root.columnconfigure(index=1)
-    root.columnconfigure(index=2)
-    root.columnconfigure(index=3)
-    root.columnconfigure(index=4)
-    root.rowconfigure(index=1)
-
-    letter_size = 192
-    char_row_num = 1
-    sub_row = 1
-
-    interface.do_inputs('Reload Exam', 0, 0, 'W', 0, refresh)
-    interface.do_labels('Change Size Font', 2, 0, 'WE')
-    interface.do_inputs('-', 2, 0, 'W', 120, None)
-    interface.do_inputs('+', 2, 0, 'E', 120, None)
-    interface.do_dropdown('Select Font', 4, 0, 'E', 0)
+def do_eye_exam():
+    interface.do_frame(840, 31, 5, None, 0)
+    interface.do_inputs('Reload', 0, 0, 'NW', 0, lambda:interface.do_reload_chart())
+    interface.do_labels('Change Size Font', 'Black', 'light blue', 1, 0, 'N')
+    interface.do_dropdown('Select Font', 2, 0, 'NE', 0)
     interface.do_graphics()
-    interface.do_chart_grid()
+    interface.do_row_numbers()
+    interface.do_chart_row_grid(font_size)
+    interface.do_inputs('-', 1, 0, 'NE', 120, lambda: interface.do_font_size(1))
+    interface.do_inputs('+', 1, 0, 'NW', 120, lambda: interface.do_font_size(0))
+    
 
-
-    root.mainloop()
-
-
-interface.do_inputs('Reload Exam', 0, 0, 'W', 0, refresh)
-interface.do_labels('Change Size Font', 2, 0, 'WE')
-interface.do_inputs('-', 2, 0, 'W', 120, None)
-interface.do_inputs('+', 2, 0, 'E', 120, None)
-interface.do_dropdown('Select Font', 4, 0, 'E', 0)
-interface.do_graphics()
-interface.do_chart_grid()
+do_eye_exam()
 
 
 root.mainloop()
