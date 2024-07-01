1e67f6d086ba076b4e8d5faa37a615ee4129b8d8
aurel84 <johnhawkins3d@gmail.com>
2024-07-01 14:44:54 -0700

    Updated SWAPI_Project.py
    * refined subject scraping process by getting json value of 'count' in the swapi url and using that to return correct number of entries
    * ensured that details in stats and info that return a url, instead returns the key value of that stat (such as character: name) instead of url for starring chars in movies
    * fixed some typos
    * removes some variables that were no longer used.

--- SWAPI_Project.py
+++ SWAPI_Project.py
@@ -7,19 +7,19 @@ in Print Statements.  Interaction is done through Inputs.
 Requirements: Python 3.12.3 (or greater)
 By John Hawkins | johnhawkins3d@gmail.com | linkedin.com/in/johnhawkins3d 
 """
-
+# note: change script name to main.py before running it
 
 import urllib.error
 import urllib.request, json
 import requests
 
 
-main_url = "https://swapi.dev/api/"
+MAIN_URL = "https://swapi.dev/api/"
 topics_list = []  # what's used to store the tpics in swapi.dev/api/*
-subjects_list = []
-tries = 30 # im using this to parse number of 404s in SWAPIs json 
+subjects_list = [] # what's printed after selecting a topic
 count = 1
 index = 1
+valid = 1
 
 
 # Using this def to grab the main topics, and then the subjects derived from those topics
@@ -35,7 +35,7 @@ def get_url_info(site_url: str):
 
 # Using this def to pick a int that corresponds to an index from topic and subjects picked.
 # in addition, since indexs start from 0, but user seletion start from 1,
-# Im subtracking -1 from user inptu to match the index chosen.
+# then subtracking -1 from user inptu to match the index chosen.
 def pick_selection(selection: int, url_list: str):
 
     while True:
@@ -45,7 +45,7 @@ def pick_selection(selection: int, url_list: str):
 
             if (selection - 1) in range(
                 len(url_list)
-            ):  # i did a subtract 1 from user's picked index it matches the list index
+            ):  # Subtracking 1 from user's picked index so it matches the list index
                 break
 
             else:
@@ -56,32 +56,46 @@ def pick_selection(selection: int, url_list: str):
 
     return selection
 
-
-if __name__ == "__main__":
-    # Im requesting the url info from SWAPI, which contains the topics, and appending them
+if __name__ == "__main__": # note: change script name to main.py before running it
+    # Requesting the url info from SWAPI, which contains the topics, and appending them
     # to a list to print them out to the print console.
-    site_topics = get_url_info(main_url)
+    site_topics = get_url_info(MAIN_URL)
 
     for x, y in site_topics.items():
         topics_list.append(y)
 
+
     print("Welcome to SWAPI, please pick from the following topics:")
     for x in topics_list:
         print(
-            "[" + str(index) + "] " + x[22:-1] # the index adds a [number] before the topic to make selection easier
+            "["
+            + str(index)
+            + "] "
+            + x[
+                22:-1
+            ]  # The index adds a [number] before the topic to make selection easier while
         )  # Printing out the topics to console, and slicing unecessary bits.
         index += 1
 
     pick_topic = input("Selection: ")
     picked_topic = pick_selection(pick_topic, topics_list)
     get_topic = topics_list[(int(pick_topic) - 1)]
+    get_count = get_url_info(
+        topics_list[picked_topic - 1]
+    )  # goes to the topic list main diretory, which has a count of total entries
 
-    # I added this part in, because SWAPIs dictionaries iterate through numbers for each topic or subjects,
-    # but ran into an issue where some numbers are blank and return 404, while the following are good.
-    # So I had the python script give a max try amounts as it iterates through the list.  If a certain amount of
-    # 404 requests are reached then it will stop printing out results.
-    
-    while tries != 0:
+    print(
+        "Thanks for picking the topic "
+        + get_topic[22:-1]
+        + " - we found "
+        + str(get_count["count"])
+        + " entries!  Now select from the following subjects: "
+    )
+
+
+    # Once selection is made, iterates through the json list.  Since some entries return 200, or 404, only counts twoard
+    # The total valid entries (i.e. return 200) up to the official count from get_count, and ignores the 404 ones.
+    while valid < get_count["count"] + 1:
 
         test = topics_list[picked_topic - 1] + str(count) + "/"
         r = requests.head(test)
@@ -90,51 +104,51 @@ if __name__ == "__main__":
 
             subjects_list.append(topics_list[picked_topic - 1] + str(count) + "/")
 
+            count += 1
+            valid += 1
+
         else:
-            tries -= 1
+            count += 1
 
-            if tries == 0:
-                break
 
-        count += 1
-
-    print(
-        "Thanks for picking the topic "
-        + get_topic[22:-1]
-        + "!  Now select from the following subjects: "
-    )
-
-    # I wante to parse the data cleanly and only get the names or titles returned instead of the
-    # whole json or dict file, which is not very user-readable in console.  # SWAPI only seems to
-    # have either a 'name' or 'title' for most of the subjects in the topics covered.\
-    
-    index = 1 # resetting the index back to 1 here for subjects
-    
+    # Parses the data cleanly and only gets the names or titles returned instead of the
+    # whole json or dict file, which is not very user-readable in console.
+    # SWAPI only seems to have either a 'name' or 'title' for most of the subjects in the topics covered.
+    index = 1
     for x in subjects_list:
-        with urllib.request.urlopen(x) as url:
-            data = json.load(url)
+        print_subjects = get_url_info(x)
 
-        for x, y in data.items():
+        for x, y in print_subjects.items():
             if x == "name" or x == "title":
-                print("[" + str(index) + "] " + y) # the index adds a [number] before the subject to make selection easier
+                print(
+                    "[" + str(index) + "] " + y
+                )  # the index adds a [number] before the subject to make selection easier
         index += 1
 
     pick_subject = input("Selection: ")
     picked_subject = pick_selection(pick_subject, subjects_list)
+    get_subjects = get_url_info(subjects_list[picked_subject - 1])
 
-    with urllib.request.urlopen(subjects_list[picked_subject - 1]) as url:
-        data = json.load(url)
 
     try:
-        print("Retrieving Info and Stats for " + data["name"] + ".")
+        print("Retrieving Info and Stats for " + get_subjects["name"] + ".")
 
     except:
-        print("Retrieving Info and Stats for " + data["title"] + ".")
+        print("Retrieving Info and Stats for " + get_subjects["title"] + ".")
 
     # this is just breaking down the data in the returned json format to something a little
     # cleaner that outputs in the console.
-    for x, y in data.items():
-        if y != None or y != "[]":
+    for x, y in get_subjects.items():
+        try:
+            for i in y:
+                get_stats_info = get_url_info(i)
+
+                try:
+                    print(x + ":", get_stats_info["name"])
+                except:
+                    print(x + ":", get_stats_info["title"])
+
+        except:
             print(x + ":", y)
 
     exit
